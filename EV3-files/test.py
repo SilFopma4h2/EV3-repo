b11e7ad3ea221d4faf53d516b56e5dff135ba7d9
Silich123 <sil.fopma@icloud.com>
2025-12-28 21:26:38 +0100

    Add basic motor control for EV3 robot

--- EV3-files/test.py
+++ EV3-files/test.py
@@ -1,1 +1,14 @@
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Port
+from pybricks.tools import wait
 
+ev3 = EV3Brick()
+
+motor_links = Motor(Port.B)
+motor_rechts = Motor(Port.C)
+
+while True:
+    motor_links.run(300)
+    motor_rechts.run(300)
+    wait(100)
